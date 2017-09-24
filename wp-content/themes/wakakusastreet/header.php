99e2283d18378ac6bd178108d8b4bb2d636aa16f
Makoto Otsuka <mametra2580@gmail.com>
2017-09-24 17:53:20 +0900

    タイトル調整

--- wp-content/themes/wakakusastreet/header.php
+++ wp-content/themes/wakakusastreet/header.php
@@ -2,7 +2,9 @@
 <html lang="ja">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php if(wp_title()) echo wp_title(); ?> 若草通商店街</title>
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
     <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
@@ -15,7 +17,6 @@
         <div id="header-title">
           <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
             <h2>若草通商店街振興組合</h2>
-            <p>WakakusaStreet</p>
           </a>
         </div>
         <div id="header-menu">
