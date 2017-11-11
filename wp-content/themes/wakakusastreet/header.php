63a3f3499434832690d998419c4e448f0c385025
Makoto Otsuka <mametra2580@gmail.com>
2017-11-11 22:25:49 +0900

    header作成

--- wp-content/themes/wakakusastreet/header.php
+++ wp-content/themes/wakakusastreet/header.php
@@ -11,6 +11,7 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+    <script src="https://code.jquery.com/jquery-2.2.3.js"></script>
 <!--
     <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
     <script src="https://code.jquery.com/jquery-2.2.3.js"></script>
@@ -18,20 +19,27 @@
 -->
   </head>
   <body>
-    <header>
-      <div class="title">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-          <h2>若草通商店街振興組合</h2>
-        </a>
-      </div>
-      <div class="menu">
-        <ul>
-          <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">トップ</a></li>
-          <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">商店街について</a></li>
-          <li><a href="<?php echo esc_url( home_url( '/shoplist' ) ); ?>">お店一覧</a></li>
-          <li><a href="<?php echo esc_url( home_url( '/empty-shoplist' ) ); ?>">空き店舗一覧</a></li>
-          <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">アクセス</a></li>
-        </ul>
+    <header id="top-head">
+      <div class="inner">
+        <div id="mobile-head">
+          <h1 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">若草通商店街振興組合</a></h1>
+          <div id="nav-toggle">
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+        </div>
+        <nav id="global-nav">
+          <ul>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">トップ</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">商店街について</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/shoplist' ) ); ?>">お店一覧</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/empty-shoplist' ) ); ?>">空き店舗一覧</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">アクセス</a></li>
+          </ul>
+        </nav>
       </div>
     </header>
 <?php wp_head(); ?>
